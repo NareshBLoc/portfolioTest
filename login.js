0776d87f84e0d457cf37c3343dc9d282af5ec7b1
NareshBLoc <nareshgdkp@gmail.com>
2025-02-06 12:57:22 +0530

    Code commited

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 function check(){
     var name = document.getElementById("name").value;
     var pwd = document.getElementById("pwd").value;
-    if(name.toUpperCase() === "NARESH" && pwd.toUpperCase() === "SUCCESSFULL")
+    if(name.toUpperCase() === "ADMIN" && pwd.toUpperCase() === "ADMIN")
         alert("Welcome my liege!!");
     else
         alert("Cease your actions peasant!");
